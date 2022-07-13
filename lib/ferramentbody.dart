7a4d2a7f87b45aeb1009dd252e3547bed70d6b1b
Eva Reboucas <evinhasjc@gmail.com>
2022-07-13 10:49:26 -0300

    a construção do treView e adicionando as classificações.

--- lib/ferramentbody.dart
+++ lib/ferramentbody.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_simple_treeview/flutter_simple_treeview.dart';
 
-Container body() {
-  return Container(
-    color: Colors.orange,
-  );
+// Container body() {
+//   return Container(
+//       color: Colors.orange,
+//       child: TreeView(
+//         nodes: [
+//           TreeNode(content: Text("Texto Principal"), children: [
+//             TreeNode(content: Text("Texto Primeiro")),
+//             TreeNode(content: Text("Text Secundário"))
+//           ])
+//         ],
+//       ));
+// }
+
+TreeView body() {
+  return TreeView(nodes: [
+    TreeNode(
+      content: Text("A 100 - Excrescências/Saliências Metálicas"),
+      children: [TreeNode(content: Text("1")), TreeNode(content: Text("2"))],
+    ),
+    TreeNode(
+      content: Text("B 100 - Cavidades/Vazios"),
+      children: [TreeNode(content: Text("1")), TreeNode(content: Text("2"))],
+    ),
+    TreeNode(
+      content: Text("C 100 - Descontinuidades do Material"),
+      children: [TreeNode(content: Text("1")), TreeNode(content: Text("2"))],
+    ),
+    TreeNode(
+      content: Text("D 100 - Defeitos Superficiais"),
+      children: [TreeNode(content: Text("1")), TreeNode(content: Text("2"))],
+    ),
+    TreeNode(
+      content: Text("E 100 - Peça Incompleta"),
+      children: [TreeNode(content: Text("1")), TreeNode(content: Text("2"))],
+    ),
+    TreeNode(
+      content: Text("F 100 - Dimensões ou Formas Incorretas"),
+      children: [TreeNode(content: Text("1")), TreeNode(content: Text("2"))],
+    ),
+    TreeNode(
+      content: Text("G 100 - Inclusões ou Anomalias na Estrutura"),
+      children: [TreeNode(content: Text("1")), TreeNode(content: Text("2"))],
+    )
+  ]);
 }
