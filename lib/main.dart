c3bbb825942428e75946f13d58b25f5da90e0479
Eva Reboucas <evinhasjc@gmail.com>
2022-07-18 18:41:54 -0300

    adicionamos scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:drfundicaoapp/bottomNavigationBar.dart';
 import 'package:drfundicaoapp/ferramentaappBar.dart';
-import 'package:drfundicaoapp/ferramentbody.dart';
 import 'package:drfundicaoapp/ferramentdrawer.dart';
 // import 'package:drfundicaoapp/ferramentbody.dart';
 
 import 'package:drfundicaoapp/floatAction.dart';
 import 'package:drfundicaoapp/persistentFooterButtons.dart';
 import 'package:flutter/material.dart';
+import 'package:scroll_to_id/scroll_to_id.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,10 +18,34 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(primaryColor: Colors.blue),
         home: Scaffold(
             appBar: appBar(),
-            body: body(),
+            body: eva(),
             drawer: drawer(context),
             floatingActionButton: floatAction(),
             bottomNavigationBar: bottomNavigationBar(),
             persistentFooterButtons: persistentFooterButtons()));
+  }
+
+  eva() {
+    final scrollController = ScrollController();
+    ScrollToId scrollToId = ScrollToId(scrollController: scrollController);
+    return InteractiveScrollViewer(
+      scrollToId: scrollToId,
+      children: <ScrollContent>[
+        ScrollContent(
+            id: 'a',
+            child: Container(
+              color: Colors.red,
+              width: 300,
+              height: 200,
+            )),
+        ScrollContent(
+            id: 'b',
+            child: Container(
+              color: Colors.green,
+              width: 300,
+              height: 200,
+            )),
+      ],
+    );
   }
 }
