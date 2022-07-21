a23dd8f1662f9e5f0c72c6fe4a64d66d06a2e90c
Eva Reboucas <evinhasjc@gmail.com>
2022-07-21 13:38:11 -0300

    adicionamos texto nas arvorés

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:drfundicaoapp/ferramentdrawer.dart';
 import 'package:drfundicaoapp/floatAction.dart';
 import 'package:drfundicaoapp/persistentFooterButtons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_simple_treeview/flutter_simple_treeview.dart';
+import 'package:scroll_to_id/scroll_to_id.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,9 +17,101 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(primaryColor: Colors.blue),
         home: Scaffold(
             appBar: appBar(),
-            body: body3(),
+            body: body1(),
             drawer: drawer(context),
             floatingActionButton: floatAction(context),
             persistentFooterButtons: persistentFooterButtons()));
+  }
+
+  body1() {
+    final scrollController = ScrollController();
+    ScrollToId scrollToId = ScrollToId(scrollController: scrollController);
+    return InteractiveScrollViewer(
+      scrollToId: scrollToId,
+      children: <ScrollContent>[
+        ScrollContent(
+            id: 'a',
+            child: Container(
+              child: TreeView(nodes: [
+                TreeNode(
+                  content: Text("A 100 - Excrescências/Saliências Metálicas"),
+                  children: [
+                    TreeNode(content: Text("A111-Rebarba")),
+                    TreeNode(content: Text("A112-Veiamento")),
+                    TreeNode(
+                        content:
+                            Text("A113-Rebarbas devido ao molde trincado ")),
+                    TreeNode(content: Text("A114-Escamas de ângulo ")),
+                    TreeNode(content: Text("A115-Veiamento de ângulo ")),
+                    TreeNode(content: Text("A123-Molde trincado ")),
+                    TreeNode(content: Text("A211-Inchamento")),
+                    TreeNode(content: Text("A212-Erosão ou Lavagem")),
+                    TreeNode(content: Text("A213-Atrito")),
+                    TreeNode(content: Text("A221-Molde quebrado")),
+                    TreeNode(
+                        content: Text(
+                            "A222-Levantamento de macho ou de bolo trincado ")),
+                    TreeNode(
+                        content:
+                            Text(" A223-Deslocamento ou flutuação de areia ")),
+                    TreeNode(
+                        content: Text(
+                            "A224-Esmagamento de arestas, quebra de cantos ")),
+                    TreeNode(
+                        content: Text("A225-Dilatação e quebra de ângulo ")),
+                    TreeNode(content: Text("A226-Macho quebrado "))
+                  ],
+                ),
+                TreeNode(
+                  content: Text("B 100 - Cavidades/Vazios"),
+                  children: [
+                    TreeNode(content: Text("1")),
+                    TreeNode(content: Text("2"))
+                  ],
+                ),
+                TreeNode(
+                  content: Text("C 100 - Descontinuidades do Material"),
+                  children: [
+                    TreeNode(content: Text("1")),
+                    TreeNode(content: Text("2"))
+                  ],
+                ),
+                TreeNode(
+                  content: Text("D 100 - Defeitos Superficiais"),
+                  children: [
+                    TreeNode(content: Text("1")),
+                    TreeNode(content: Text("2"))
+                  ],
+                ),
+                TreeNode(
+                  content: Text("E 100 - Peça Incompleta"),
+                  children: [
+                    TreeNode(content: Text("1")),
+                    TreeNode(content: Text("2"))
+                  ],
+                ),
+                TreeNode(
+                  content: Text("F 100 - Dimensões ou Formas Incorretas"),
+                  children: [
+                    TreeNode(content: Text("1")),
+                    TreeNode(content: Text("2"))
+                  ],
+                ),
+                TreeNode(
+                  content: Text("G 100 - Inclusões ou Anomalias na Estrutura"),
+                  children: [
+                    TreeNode(
+                        content: IconButton(
+                            iconSize: 50,
+                            icon: Icon(Icons.remove_circle),
+                            color: Colors.teal,
+                            padding: const EdgeInsets.all(0.0),
+                            onPressed: () {}))
+                  ],
+                )
+              ]),
+            )),
+      ],
+    );
   }
 }
